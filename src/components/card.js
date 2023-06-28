55e9bff1fc686a04455e4e49de0fc47e00030255
Edge <edgeo0314@gmail.com>
2023-06-28 14:42:24 -0600

    Optimized the code of the character cards. Changed styling of pagination buttons

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,8 @@
 var pinyin_v1 = require("pinyin-tone");
 
 function CharacterCard({ hanzi, cmn, yue, jkun, jon, def }) {
+  const formatText = (text) =>
+    String(text).charAt(0).toUpperCase() + String(text).slice(1).toLowerCase();
   return (
     <div className="character-card">
       <h2>{hanzi}</h2>
@@ -8,41 +10,25 @@ function CharacterCard({ hanzi, cmn, yue, jkun, jon, def }) {
         <p>
           Mandarin:{" "}
           {cmn
-            ? String(pinyin_v1(String(cmn).toLowerCase()))
-                .charAt(0)
-                .toUpperCase() +
-              String(pinyin_v1(String(cmn).toLowerCase()))
-                .slice(1)
-                .toLowerCase()
-                .replace(/\s+/g, ", ")
+            ? formatText(pinyin_v1(String(cmn).toLowerCase())).replace(
+                /\s+/g,
+                ", "
+              )
             : "No reading"}
         </p>
         <p>
           Cantonese:{" "}
-          {yue
-            ? String(yue).charAt(0).toUpperCase() +
-              String(yue).slice(1).toLowerCase().replace(/\s+/g, ", ")
-            : "No reading"}
+          {yue ? formatText(yue).replace(/\s+/g, ", ") : "No reading"}
         </p>
         <p>
           Japanese Kun'yomi:{" "}
-          {jkun
-            ? String(jkun).charAt(0).toUpperCase() +
-              String(jkun).slice(1).toLowerCase().replace(/\s+/g, ", ")
-            : "No reading"}
+          {jkun ? formatText(jkun).replace(/\s+/g, ", ") : "No reading"}
         </p>
         <p>
           Japanese On'yomi:{" "}
-          {jon
-            ? String(jon).charAt(0).toUpperCase() +
-              String(jon).slice(1).toLowerCase().replace(/\s+/g, ", ")
-            : "No reading"}
+          {jon ? formatText(jon).replace(/\s+/g, ", ") : "No reading"}
         </p>
-        <p className="definition">
-          Definition:{" "}
-          {String(def).charAt(0).toUpperCase() +
-            String(def).slice(1).toLowerCase()}
-        </p>
+        <p className="definition">Definition: {formatText(def)}</p>
       </div>
     </div>
   );
