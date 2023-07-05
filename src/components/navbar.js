5e4c51776aa4f4afd3e5134625997545e7fda414
Edge <edgeo0314@gmail.com>
2023-07-05 17:22:44 -0600

    Added responsive CSS for all screens + small layout adjustments

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [contentShown, setContentShown] = useState(false);
+
+  const handleDropdown = () => {
+    setContentShown(!contentShown);
+  };
   return (
     <div id="nav-bar">
       <h2 id="title">汉字数据库 The Hanzi Database</h2>
@@ -21,6 +27,35 @@ function NavBar() {
           </Link>
         </li>
       </ul>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={handleDropdown}>
+          Menu
+        </button>
+        <div
+          className={
+            contentShown ? "dropdown-content" : "dropdown-content shown"
+          }
+        >
+          <ul>
+            <li>
+              {" "}
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <a className="nav-link" href="#b">
+                Info
+              </a>
+            </li>
+            <li>
+              <Link to="/search" className="nav-link">
+                Database
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
